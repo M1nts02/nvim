6af073deaa0baaceb471059bc48d198df31959f4
M1nts02 <m1nts02@M1nts02s-Air.lan>
2024-01-03 13:24:05 +0800

    Add .gitignore

--- lua/custom.lua
+++ lua/custom.lua
@@ -5,9 +5,9 @@
 --vim.g.light_theme = "akane-light"
 --vim.g.dark_theme = "akane-dark"
 --vim.g.lualine_theme = "akane"
-vim.o.shell = "/opt/homebrew/bin/nu"
+--vim.o.shell = "/opt/homebrew/bin/nu"
 
-vim.g.lldb = "/opt/homebrew/opt/llvm/bin/lldb-vscode"
+--vim.g.lldb = "/opt/homebrew/opt/llvm/bin/lldb-vscode"
 
 lsp_servers = {
   "bashls",
@@ -23,7 +23,7 @@ lsp_servers = {
 }
 
 if vim.g.neovide then
-  vim.o.guifont = "RecMonoCasual Nerd Font:h14"
-  vim.g.neovide_floating_shadow = false
-  vim.g.neovide_transparency = 1
+  --vim.o.guifont = "RecMonoCasual Nerd Font:h14"
+  --vim.g.neovide_floating_shadow = false
+  --vim.g.neovide_transparency = 1
 end
