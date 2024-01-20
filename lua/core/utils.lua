343efa2afb1e02e205575f30dcb60511f57d5fc7
M1nts02 <m1nts02@M1nts02s-Air.lan>
2024-01-21 02:14:52 +0800

    Delete useless function

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,45 +1,9 @@
 local M = {}
 
-local os_name = jit.os
-M.is_mac = os_name:find "OSX" ~= nil
-M.is_linux = os_name:find "Linux" ~= nil
-M.is_windows = os_name:find "Windows" ~= nil
-
 -- connect path
 M.path_join = function(...)
-  local sep = M.is_windows and "\\" or "/"
+  local sep = vim.g.is_windows and "\\" or "/"
   return table.concat({ ... }, sep)
-end
-
--- connect tables
-M.merge_tables = function(...)
-  local tabs = { ... }
-  if not tabs then
-    return {}
-  end
-  local origin = tabs[1]
-  for i = 2, #tabs do
-    if origin then
-      if tabs[i] then
-        for _, v in pairs(tabs[i]) do
-          table.insert(origin, v)
-        end
-      end
-    else
-      origin = tabs[i]
-    end
-  end
-  return origin
-end
-
--- set map
-M.set_map = function(map)
-  for _, m in ipairs(map) do
-    if m[4] == nil then
-      m[4] = { noremap = true }
-    end
-    vim.keymap.set(m[1], m[2], m[3], m[4])
-  end
 end
 
 -- copy file
