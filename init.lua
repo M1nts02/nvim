2a9f428fc7e5d74cd9d3c944975c1dac622d72a3
M1nts02 <m1nts02@M1nts02s-Air.lan>
2024-01-28 15:54:18 +0800

    Move bootstrap to init.lua

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@ vim.g.is_mac = os_name:find "OSX" ~= nil
 vim.g.is_linux = os_name:find "Linux" ~= nil
 vim.g.is_windows = os_name:find "Windows" ~= nil
 
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local confpath = vim.fn.stdpath "config"
+
 -- Load custom.lua
 local result = require "core.load_custom"
 
@@ -10,24 +13,63 @@ if not result then
   return
 end
 
+-- Load options
 require "core.options"
-require "core.bootstrap"
 
--- Lazy-load
-vim.api.nvim_create_autocmd("UIEnter", {
-  callback = function()
-    require "core.mapping"
-  end,
-})
+-- Don't load shada
+local shada = vim.o.shada
+vim.o.shada = ""
 
--- Large file
-vim.api.nvim_create_autocmd("BufReadPre", {
-  callback = function()
-    local filesize = vim.fn.getfsize(vim.fn.expand "%")
-    if filesize > 50000000 then
-      vim.o.syntax = false
-      vim.o.number = false
-      vim.o.relativenumber = false
-    end
-  end,
-})
+-- Check plugin manager
+if not vim.loop.fs_stat(lazypath) then
+  print "Downloading ..."
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  }
+end
+
+-- Load lazy.nvim
+vim.opt.runtimepath:prepend(lazypath)
+
+require("lazy").setup {
+
+  -- Plugins manager
+  { "folke/lazy.nvim", version = "*" },
+
+  -- Load plugins
+  { import = "plugins" },
+
+  -- Lazy-load
+  {
+    name = "lazy-load",
+    dir = confpath,
+    event = "VeryLazy",
+    config = function()
+      -- Load shada
+      vim.o.shada = shada
+      pcall(vim.cmd.rshada, { bang = true })
+      -- Load mapping
+      require "core.mapping"
+    end,
+  },
+
+  -- Large file
+  {
+    name = "large-file",
+    dir = confpath,
+    event = "BufReadPre",
+    config = function()
+      local filesize = vim.fn.getfsize(vim.fn.expand "%")
+      if filesize > 50000000 then
+        vim.o.syntax = false
+        vim.o.number = false
+        vim.o.relativenumber = false
+      end
+    end,
+  },
+}
