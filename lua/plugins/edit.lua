66cb3d8dc2214c27c6989107ffce8fba14afa405
= <=>
2024-02-29 13:53:52 +0800

    Delete treesitter

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -12,34 +12,6 @@ return {
     },
   },
 
-  -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    enabled = not vim.g.is_windows,
-    event = { "BufRead", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate",
-    init = function()
-      -- Enable highlight
-      vim.api.nvim_create_autocmd("BufReadPost", {
-        pattern = { "*.kdl", "*.md", "*.nu", "*.toml", "*.yaml" },
-        command = "TSBufEnable highlight",
-      })
-    end,
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-    opts = {
-      ensure_installed = { "c", "lua", "markdown", "org", "query", "vim", "vimdoc" },
-      sync_install = false,
-      auto_install = true,
-      highlight = { enable = false, additional_vim_regex_highlighting = false },
-    },
-    dependencies = {
-      { "LhKipp/nvim-nu", main = "nu", opts = { use_lsp_features = false } },
-    },
-  },
-
   -- Cursor jump
   {
     "ggandor/flit.nvim",
